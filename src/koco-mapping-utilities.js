ac352fb40ae6b2945854f1378409f70e9e9c1240
David D'Amico <david.damico@cbc.ca>
2015-04-28 14:52:54 -0400

    initial commit of component creation work

---
+++ src/koco-mapping-utilities.js
@@ -0,0 +1,51 @@
+// Copyright (c) CBC/Radio-Canada. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+define(['knockout'],
+    function(ko) {
+        'use strict';
+
+        function KnockoutMappingUtilities() {}
+
+        KnockoutMappingUtilities.prototype.mapAsObservableValueObjects = function(mapping, complexAttributeNames) {
+            var self = this;
+
+            for (var i = 0; i < complexAttributeNames.length; i++) {
+                self.mapAsObservableValueObject(mapping, complexAttributeNames[i]);
+            }
+        };
+
+        KnockoutMappingUtilities.prototype.mapAsObservableValueObject = function(mapping, complexAttributeName) {
+            mapping[complexAttributeName] = {
+                update: asObservableValueObject
+            };
+        };
+
+        KnockoutMappingUtilities.prototype.toJS = function(x) {
+            if(!x){
+                return x;
+            }
+
+            var result = ko.unwrap(x);
+
+            if(!result){
+                return result;
+            }
+
+            if(result.__ko_mapping__){
+                return ko.mapping.toJS(result);
+            }
+
+            return ko.toJS(result);
+        };
+
+        function asObservableValueObject(options) {
+            if (!options.data) {
+                return null;
+            }
+
+            return ko.observable(options.data);
+        }
+
+        return new KnockoutMappingUtilities();
+    });
