eabc45f00340916ceace1b41a550cd3ac676a3cc
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-24 14:45:30 -0400

    update to koco2

---
+++ dist/koco-mapping-utilities.js
@@ -0,0 +1,56 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _knockout = require('knockout');
+
+var _knockout2 = _interopRequireDefault(_knockout);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function KnockoutMappingUtilities() {} // Copyright (c) CBC/Radio-Canada. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+KnockoutMappingUtilities.prototype.mapAsObservableValueObjects = function (mapping, complexAttributeNames) {
+    var self = this;
+
+    for (var i = 0; i < complexAttributeNames.length; i++) {
+        self.mapAsObservableValueObject(mapping, complexAttributeNames[i]);
+    }
+};
+
+KnockoutMappingUtilities.prototype.mapAsObservableValueObject = function (mapping, complexAttributeName) {
+    mapping[complexAttributeName] = {
+        update: asObservableValueObject
+    };
+};
+
+KnockoutMappingUtilities.prototype.toJS = function (x) {
+    if (!x) {
+        return x;
+    }
+
+    var result = _knockout2.default.unwrap(x);
+
+    if (!result) {
+        return result;
+    }
+
+    if (result.__ko_mapping__) {
+        return _knockout2.default.mapping.toJS(result);
+    }
+
+    return _knockout2.default.toJS(result);
+};
+
+function asObservableValueObject(options) {
+    if (!options.data) {
+        return null;
+    }
+
+    return _knockout2.default.observable(options.data);
+}
+
+exports.default = new KnockoutMappingUtilities();
